bff4aba8386055d35d480a18c620e2282cb82297
piraye-bulut <140714145+piraye-bulut@users.noreply.github.com>
2024-06-16 02:04:59 +0300

    Landing page
    g React js, html,javascript and Tailwindcss

---
+++ soundmate2/index.js
@@ -0,0 +1,16 @@
+
+tailwind.config = {
+    darkmode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                "logo":{
+                    700:"#1C59AE",
+                    800:"#114a9a",
+
+                }
+                
+            }
+        }
+    }
+}
